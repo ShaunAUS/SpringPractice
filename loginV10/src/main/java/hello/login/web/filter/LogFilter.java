14894bff8fc5dab37e0359dd12a0b4fcc19827d4
ShaunAUS <86937655+ShaunAUS@users.noreply.github.com>
2022-10-18 22:56:42 +0900

    loginV10
    인터셉터 인증체크

---
+++ loginV10/src/main/java/hello/login/web/filter/LogFilter.java
@@ -0,0 +1,48 @@
+package hello.login.web.filter;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+public class LogFilter implements Filter {
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("log filter init");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        log.info("log filter doFilter");
+
+        //요청
+        //HttpServeltRequest 자식이 기능 더많아서 캐스팅 해줌
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+
+        //모든 URL 로그 남기기
+        String requestURI = httpRequest.getRequestURI();
+
+        //요청 구분하기 위해
+        String uuid = UUID.randomUUID().toString();
+
+        try {
+            log.info("REQUEST  [{}][{}]", uuid, requestURI);
+            chain.doFilter(request, response);   // FIXME 이게 있어야 다음 필터로 넘어감 없으면  -> 서블릿 ->컨트룰러 호출
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            log.info("RESPONSE [{}][{}]", uuid, requestURI);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        log.info("log filter destroy");
+    }
+}
